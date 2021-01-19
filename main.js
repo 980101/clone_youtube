bead97d1e7e1ce63aa116f1a2d7a230086c0f188
hj <hyejuggang@gmail.com>
2021-01-19 20:37:28 +0900

    Update abbreviated title to expand

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
-const moreBtn = document.querySelector('.infoAndUpnext .info .titleAndButton .moreBtn');
-const title = document.querySelector('.infoAndUpnext .info .titleAndButton .title');
+'use strict'
+
+const moreBtn = document.querySelector('.info .titleAndButton .moreBtn');
+const title = document.querySelector('.info .titleAndButton .title');
 
 moreBtn.addEventListener('click', ()=>{
     moreBtn.classList.toggle('clicked');
     title.classList.toggle('clamp');
+});
+
+/* responsive effect .moreBtn in .upNext */
+const upNext = document.querySelector('.upNext .item');
+const item_moreBtn = document.querySelector('.upNext .item .moreBtn');
+
+upNext.addEventListener('mouseenter', ()=>{
+    item_moreBtn.classList.add('active');
+});
+
+upNext.addEventListener('mouseleave', ()=>{
+    item_moreBtn.classList.remove('active');
 });
